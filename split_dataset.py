4479ed21fbe3e26c2cc06b67b39706325182c730
zxm20243 <zxm20243@users.noreply.github.com>
2015-01-10 05:14:46 +0800

    split_dataset.py only generate training/testing file list

--- split_dataset.py
+++ split_dataset.py
@@ -1,49 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import os
 import sys
 import random
-import shutil
-import itertools
+from os import getcwd, listdir
+from os.path import isfile, join
 
 v = 10
-rootpath = os.path.join(os.getcwd(),  sys.argv[1])
-trainpath = os.path.join(os.getcwd(), sys.argv[2])
-testpath = os.path.join(os.getcwd(),  sys.argv[3])
-
-def chunks(l, n):
-    for i in range(0, len(l), n):
-        yield l[i:i+n]
-
-# Clean old training set and test set
-if os.path.isdir(trainpath): shutil.rmtree(trainpath)
-if os.path.isdir(testpath): shutil.rmtree(testpath)
-os.mkdir(trainpath), os.mkdir(testpath)
-
-for dirpath, dirnames, filenames in os.walk(rootpath):
-    # Copy each directory
-    for dirname in dirnames:
-        src = os.path.join(dirpath, dirname)
-        os.mkdir(src.replace(rootpath, trainpath, 1))
-        os.mkdir(src.replace(rootpath, testpath, 1))
-
-    # random shuffle filelist
-    if len(filenames)/v<=0: continue
-    random.shuffle(filenames)
-    partitions = list(chunks(filenames, len(filenames) // v))
-
-    # Random choose a fold to generate testSet and trainSet
-    r = random.randrange(1,v,1)
-    trainSet = list(itertools.chain.from_iterable(partitions[:r]+partitions[(r+1):]))
-    testSet = partitions[r]
-
-    #print(dirpath)
-    for filename in trainSet:
-        src = os.path.join(dirpath, filename)
-        dst = src.replace(rootpath, trainpath, 1)
-        shutil.copy2(src, dst)
-    for filename in testSet:
-        src = os.path.join(dirpath, filename)
-        dst = src.replace(rootpath, testpath, 1)
-        shutil.copy2(src, dst)
+rootpath = join(getcwd(), sys.argv[1])
+trainfile = open(sys.argv[2], mode='w')
+testfile = open(sys.argv[3], mode='w')
+for idx, label in enumerate(listdir(rootpath)):
+    dirpath = join(rootpath, label)
+    filelist = [ f for f in listdir(dirpath) if isfile(join(dirpath, f)) ]
+    random.shuffle(filelist)
+    test_sample = filelist[0:len(filelist):v]
+    train_sample = [f for f in filelist if f not in test_sample]
+    for f in test_sample: print(join(label, f) + " " + str(idx), file=testfile)
+    for f in train_sample: print(join(label, f) + " " + str(idx), file=trainfile)
