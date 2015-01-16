6290acca3d0b24098190d9b88ba2dc76461c512e
zxm20243 <zxm20243@users.noreply.github.com>
2015-01-16 08:07:09 +0800

    add test phase

--- naive_hist.m
+++ naive_hist.m
@@ -11,7 +11,7 @@ data = textscan(fd, '%s %d');
 fclose(fd);
 
 % Training phase
-trainFeatures = [zeros(size(data{1}, 1), w*h*c)];
+features = [zeros(size(data{1}, 1), w*h*c)];
 sumImg = zeros(80, 80, 3);
 sqrsumImg = zeros(80, 80, 3);
 for idx=1:size(data{1}, 1)
@@ -21,18 +21,35 @@ for idx=1:size(data{1}, 1)
     img = imresize(imread(fullfile(root, data{1}{idx})), [h w]);
     sumImg = sumImg + double(img);
     sqrsumImg = sqrsumImg + double(img.^2);
-    %trainFeatures = [trainFeatures ; double(reshape(img, 1, []))];
-    trainFeatures(idx,:) = double(reshape(img, 1, []));
+    features(idx,:) = double(reshape(img, 1, []));
 end
 meanVector = reshape(sumImg/size(data{1}, 1), 1, []);
 stdVector = reshape(sqrsumImg/size(data{1}, 1), 1, [])-meanVector.^2;
-trainFeatures = trainFeatures-repmat(meanVector, size(trainFeatures, 1), 1);
-trainFeatures = trainFeatures./repmat(stdVector, size(trainFeatures, 1), 1);
-trainFeatures = sparse(trainFeatures);
-trainLabel = double(data{2}(1:size(trainFeatures, 1)));
+features = features-repmat(meanVector, size(features, 1), 1);
+features = features./repmat(stdVector, size(features, 1), 1);
+features = sparse(features);
+label = double(data{2}(1:size(features, 1)));
+model = train(label, features);
 
-model = train(trainLabel, trainFeatures);
-[predict_train, train_acc, ans] = predict(trainLabel, trainFeatures, model);
-[predict_test, train_acc, ans] = predict(trainLabel, trainFeatures, model);
+
+% Testing phase
+fd = fopen(testlist);
+data = textscan(fd, '%s %d');
+fclose(fd);
+
+features = [zeros(size(data{1}, 1), w*h*c)];
+for idx=1:size(data{1}, 1)
+    if (mod(idx, 100) == 0) 
+        fprintf(1, '#%d file\n', idx);
+    end
+    img = imresize(imread(fullfile(root, data{1}{idx})), [h w]);
+    features(idx,:) = double(reshape(img, 1, []));
+end
+features = features-repmat(meanVector, size(features, 1), 1);
+features = features./repmat(stdVector, size(features, 1), 1);
+features = sparse(features);
+label = double(data{2}(1:size(features, 1)));
+
+[predict_test, test_acc, ans] = predict(label, features, model);
 save(modelpath, 'model');
 end
